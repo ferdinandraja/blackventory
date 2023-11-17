ca536b29438c8d8f4c638ff7e6ac14069efc6550
ferdinandraja <ferdinand.raja@ui.ac.id>
2023-11-17 20:38:21 +0700

    finish setengah assignment

--- lib/screen/shoplist_form.dart
+++ lib/screen/shoplist_form.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 // TODO: Import the previously created drawer
 import 'package:blackventory/widgets/left_drawer.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:blackventory/screen/menu.dart';
+import 'dart:convert';
 class ShopFormPage extends StatefulWidget {
     const ShopFormPage({super.key});
 
@@ -12,9 +16,12 @@ class _ShopFormPageState extends State<ShopFormPage> {
     final _formKey = GlobalKey<FormState>();
     String _name = "";
     int _price = 0;
+    int _amount = 0;
     String _description = "";
     @override
     Widget build(BuildContext context) {
+      final request = context.watch<CookieRequest>();
+
         return Scaffold(
   appBar: AppBar(
     title: const Center(
@@ -140,39 +147,37 @@ child: ElevatedButton(
     backgroundColor:
         MaterialStateProperty.all(Colors.indigo),
   ),
-  onPressed: () {
+onPressed: () async {
     if (_formKey.currentState!.validate()) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Product successfully saved'),
-            content: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment:
-                    CrossAxisAlignment.start,
-                children: [
-                  Text('Name: $_name'),
-                  Text('Price: $_price'),
-                  Text('Description: $_description'),
-                  // TODO: Display other values
-                ],
-              ),
-            ),
-            actions: [
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          );
-        },
-      );
-    _formKey.currentState!.reset();
+        // Send request to Django and wait for the response
+        // TODO: Change the URL to your Django app's URL. Don't forget to add the trailing slash (/) if needed.
+        final response = await request.postJson(
+        "http://127.0.0.1:8000/create-flutter/",
+        jsonEncode(<String, String>{
+            'name': _name,
+            'price': _price.toString(),
+            'amount': _amount.toString(),
+            'description': _description,
+            // TODO: Adjust the fields with your Django model
+        }));
+        if (response['status'] == 'success') {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(
+            content: Text("New product has saved successfully!"),
+            ));
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => MyHomePage()),
+            );
+        } else {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(
+                content:
+                    Text("Something went wrong, please try again."),
+            ));
+        }
     }
-  },
+},
   child: const Text(
     "Save",
     style: TextStyle(color: Colors.white),
